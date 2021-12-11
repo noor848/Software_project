38e72b6665141e97bf1346894bbe85474ce9f28b
Aya-Hamdan1 <91088526+Aya-Hamdan1@users.noreply.github.com>
2021-12-11 17:46:17 +0200

    Add files via upload

---
+++ AssignmentController.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gma;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import static jdk.nashorn.internal.runtime.JSType.toInteger;
+
+/**
+ * FXML Controller class
+ *
+ * @author ayaha
+ */
+public class AssignmentController implements Initializable {
+     SubjectController s= new SubjectController();
+     int Tid = s.teacher;
+     //int id = s.idSub;
+     String subject = s.subject;
+    // String Sub = s.assName;
+     //String assi = s.Assignment0;
+    
+    /**
+     * Initializes the controller class.
+     */
+     @FXML
+    private TextField assName;
+    private Label b;
+    @FXML
+    private TextArea details;
+    @FXML
+    void add(MouseEvent event)  { 
+       
+        int id =0;
+        try{
+        
+       Connect c=new Connect();
+       Connection con= c.connect_datbade();
+        String assName0=assName.getText();//duration`, `id`, `id_teacher`
+        //String password1;
+        int duration =toInteger(details.getText()) ;
+        //JOptionPane.showMessageDialog(null, ":"+duration);
+        
+      	
+       String q0 = "SELECT * FROM assignments WHERE name ='"+assName0+"'&& sub_name ='"+subject+"'&& id_teacher='"+Tid+"'";//SELECT `name`, `duration`, `id`, `id_teacher` FROM `assignments` WHERE `name`="aya";
+         java.sql.Statement statment=con.createStatement( );
+       ResultSet rs =statment.executeQuery(q0);
+         
+             if(rs.next()==true){
+                 JOptionPane.showMessageDialog(null,"Theis assignment is already exist ");
+             }
+             else{
+                 String qq = "SELECT * FROM assignments where 1";
+                 Statement statement=con.createStatement( );
+                 ResultSet rs5=statement.executeQuery(qq);
+                 while (rs5.next()) {
+                  id=rs5.getInt("id");
+                  ++id;
+                  }
+        String q = "INSERT INTO assignments(`name`, `duration`, `id`, `id_teacher`, `sub_name`) VALUES ('"+assName.getText()+"','"+duration+"','"+id+"','"+Tid+"','"+subject+"')";
+        java.sql.Statement statement5=con.createStatement( );
+        statement5.executeUpdate(q); 
+            //  ResultSet rs=statement.executeQuery(q);
+          //rs.absolute(0); //befor and afte first an dlast row
+         /* while (rs.next()) {
+              duration = rs.getInt("duration");
+          }*/
+         JOptionPane.showMessageDialog(null,"Add SUCCESSFULLY");
+          details.setText(" ");
+          assName.setText("");
+             }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+    }
+
+    //@FXML
+    /*void delete(MouseEvent event)  {
+        try{
+     String assName0=assName.getText();
+     int Tid =1;
+    Connect c=new Connect();
+     Connection con= c.connect_datbade();
+     
+      String q0 = "SELECT * FROM assignments WHERE name ='"+assName0+"'& id_teacher ='"+Tid+"'";
+         java.sql.Statement statment=con.createStatement( );
+       ResultSet rs =statment.executeQuery(q0);
+         
+          if(rs.next()==true){
+         String q5 = "DELETE FROM assignments WHERE name ='"+assName0+"'& id_teacher ='"+Tid+"'";
+        java.sql.Statement statement5=con.createStatement( );
+        statement5.executeUpdate(q5);
+                 JOptionPane.showMessageDialog(null,"Delete SUCCESSFULLY");
+                 assName.setText("");
+                 details.setText("");
+             }
+             else{
+               JOptionPane.showMessageDialog(null,"not exist!");  
+             }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,"can't delete");
+        }
+    // String q = "SELECT * FROM assignments WHERE name ='"+assName0+"'";
+     //   Statement statement=con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+      //        ResultSet rs=statement.executeQuery(q);
+       //   rs.absolute(0); //befor and afte first an dlast row
+      /*while (rs.next()) {
+          String q5 = "DELETE FROM `assignments` WHERE `name` ='"+assName0+"'";
+        Statement statement5=con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+              ResultSet rs5=statement5.executeQuery(q5);
+      }*/
+
+
+   // }
+
+    @FXML
+    void update(MouseEvent event) {
+ try{
+     String assName0=assName.getText();
+     //int Tid =5;
+     //int id = 5;
+     int du = toInteger(details.getText());
+    Connect c=new Connect();
+     Connection con= c.connect_datbade();
+     
+      String q0 = "SELECT * FROM assignments WHERE name ='"+assName0+"'&& sub_name ='"+subject+"'&& id_teacher='"+Tid+"'";
+         java.sql.Statement statment=con.createStatement( );
+       ResultSet rs =statment.executeQuery(q0);
+         
+          if(rs.next()){
+              Statement st=con.createStatement();
+           String update ="UPDATE assignments SET name='"+assName0+"',duration='"+du+"',id='"+rs.getInt("id")+"',id_teacher='"+Tid+"' WHERE name ='"+assName0+"'&& sub_name='"+subject+"'&& id_teacher='"+Tid+"'";
+           st.executeUpdate(update);
+            JOptionPane.showMessageDialog(null,"UPDATE SUCCESSFULLY");
+             details.setText(" ");
+          assName.setText("");
+              
+          }
+          else{
+             JOptionPane.showMessageDialog(null,"not exist"); 
+          }
+ }
+ catch(Exception e){
+     JOptionPane.showMessageDialog(null,e);
+ }
+    }
+    @FXML
+    void tosub(MouseEvent event) {
+
+    }
+     @FXML
+    void bk(MouseEvent event) throws IOException {
+      ((Stage)(((Label)event.getSource()).getScene().getWindow())).close();    
+      Stage stage = new Stage();
+              
+               Parent root ;
+               root = FXMLLoader.load(getClass().getResource("subject.fxml"));
+               Scene scene = new Scene(root);  
+               stage.setScene(scene);
+               stage.setTitle("Subject");
+               stage.show();
+
+      
+    }
+     /*@FXML
+    void toGrades(MouseEvent event) throws IOException {
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close(); 
+               Stage stage = new Stage();
+               Parent root ;
+               root = FXMLLoader.load(getClass().getResource("grades.fxml"));
+               Scene scene = new Scene(root);  
+               stage.setScene(scene);
+               stage.setTitle("Grades");
+               stage.show();
+    }*/
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ Sign_inController.java
@@ -0,0 +1,208 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gma;
+
+import static gma.SubjectController.idClass;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.paint.Color;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseDragEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author zaytona
+ */
+public class Sign_inController implements Initializable {
+    public static int t_id;
+    //public static int class_id;
+    public static final ObservableList nos = FXCollections.observableArrayList();
+    @FXML 
+    private TextField email; 
+    @FXML 
+    private PasswordField pass; 
+    @FXML
+    private Label invalid;
+    int flag=0;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loginButt(ActionEvent event) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, Exception {
+        
+
+       Connect c=new Connect();
+ 
+       Connection con= c.connect_datbade();
+
+
+        
+        String email1=email.getText();
+        String password1=pass.getText();
+        
+   
+      	String q = "SELECT * FROM school_maneger";
+        Statement statement=con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+              ResultSet rs=statement.executeQuery(q);
+          rs.absolute(0); //befor and afte first an dlast row
+      while (rs.next()) {
+       
+              if(email1.equalsIgnoreCase(rs.getString("Email"))&&password1.equals(rs.getString("password")))
+       
+              {
+                  
+                
+              flag=1;
+              break;
+              }
+                
+      }
+       	String q2 = "SELECT * FROM teacher";
+        Statement statement2=con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+              ResultSet rs2=statement2.executeQuery(q2);
+          rs2.absolute(0); //befor and afte first an dlast row
+      while (rs2.next()) {
+       
+              if(email1.equalsIgnoreCase(rs2.getString("Email"))&&password1.equals(rs2.getString("password")))
+       
+              {
+                  
+               t_id =rs2.getInt("id_t");
+              flag=2;
+              /*String class0 = "SELECT * FROM tea_class WHERE id_teacher='"+rs2.getInt("id_t")+"'";
+              Statement statement3=con.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+              ResultSet rs3=statement3.executeQuery(class0);
+                if (rs3.next()){
+                    class_id = rs3.getInt("id_class");
+               String idS = "SELECT * FROM subject WHERE class_id ='"+rs3.getInt("id_class")+"'";//choose id subject 
+        Statement statement5=con.createStatement( );
+              ResultSet rs4=statement5.executeQuery(idS);
+                
+              while(rs4.next()){
+                  nos.add(rs4.getString("name"));
+                 
+              }
+             
+              //Sub.setItems(nos);*/
+              break;
+              }
+              }
+                
+      
+      
+      
+      
+      
+      
+      if(flag==1){
+        System.out.print("hello");
+  
+try {
+                Stage stage = new Stage();
+               Parent root;
+        
+            root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
+        
+               Scene scene = new Scene(root);  
+               stage.setScene(scene);
+               stage.setTitle("Admin profile");
+               stage.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();} 
+catch (IOException ex) {
+            
+        }
+
+            } 
+      if(flag==2){
+        System.out.print("hello");
+  
+try {
+                Stage stage = new Stage();
+               Parent root;
+        
+            root = FXMLLoader.load(getClass().getResource("Subject.fxml"));
+        
+               Scene scene = new Scene(root);  
+               stage.setScene(scene);
+               stage.setTitle("Subject");
+               stage.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();} 
+catch (IOException ex) {
+            
+        }
+
+            } 
+          
+
+
+      
+      
+      
+      
+      else{
+      
+JOptionPane.showMessageDialog(null, "Please try Again !");
+     
+      
+      }
+      
+      
+      
+    
+        
+        
+        
+        
+    }
+
+     @FXML
+private void exitButtonOnAction(ActionEvent event){
+
+    int c =JOptionPane.showConfirmDialog(null, "Are you sure?", "WARNING", JOptionPane.YES_NO_OPTION);
+    if (c == JOptionPane.YES_OPTION) {
+    ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();  
+} else {
+    
+}
+         }
+
+    @FXML
+    private void forgot_butt(MouseDragEvent event) {
+    }
+
+  
+}
